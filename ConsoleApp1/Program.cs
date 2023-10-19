4a1e40cc12392bab86f43e64655d2f0b57d5abb2
Zbiku <Zbiku@MSI>
2023-10-19 22:47:12 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        double BMI, m, w;
+        Console.WriteLine("Podaj swoją wagę w kilogramach: ");
+        m = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj swój wzrost w metrach: ");
+        w = double.Parse(Console.ReadLine());
+        BMI = m / (Math.Pow(w, 2));
+        Console.WriteLine("Twoje BMI wynosi: " + BMI);
+        Console.ReadKey();
+    }
+}
